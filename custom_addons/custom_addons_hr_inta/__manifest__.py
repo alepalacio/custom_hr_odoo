a29d1449dd0219865cabcfe1b2437dc8a636b483
alepalacio <alejandropalacio24@gmail.com>
2023-09-13 19:55:50 -0300

    added security rules for situacion revista 1

--- custom_addons/custom_addons_hr_inta/__manifest__.py
+++ custom_addons/custom_addons_hr_inta/__manifest__.py
@@ -8,6 +8,8 @@
     'data': [
         'security/ir.model.access.csv',
         'views/hr_employee_nro_legajo_views.xml',
+        #'views/hr_department_tipo_unidad_views.xml',
+        'views/hr_employee_situacion_revista_1_views.xml',
     ],
     'installable': True,
     'application': True,
