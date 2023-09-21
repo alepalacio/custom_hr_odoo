b00b88afb050d5b1465f13f7d6aa49a94bcf67dc
alepalacio <alejandropalacio24@gmail.com>
2023-09-20 22:32:41 -0300

    removed field for employee

--- custom_addons/custom_addons_hr_inta/models/hr_custom_employee.py
+++ custom_addons/custom_addons_hr_inta/models/hr_custom_employee.py
@@ -23,22 +23,8 @@ class HrCustomEmployee(models.Model):
         "employee_id",
         string="Formación"
     )
-    formacion_titulo_carrera_descripcion = fields.Char(
-        string="Descripción del Título de Carrera",
-        compute="_compute_titulo_carrera_descripcion",
-        store=True,
-        #readonly=True,
-        )
     grupo_familiar_id = fields.One2many(
         "hr.employee.grupo_familiar",
         "employee_id",
         string="Grupo Familiar"
     )
-    
-    @api.depends("formacion_id")
-    def _compute_titulo_carrera_descripcion(self):
-        for record in self:
-            if record.formacion_id:
-                record.formacion_titulo_carrera_descripcion = record.formacion_id.titulo_carrera_id.descripcion
-            else:
-                record.formacion_titulo_carrera_descripcion = None
