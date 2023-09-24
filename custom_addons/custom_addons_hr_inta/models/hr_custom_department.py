4a805784f8c74581543df8424de45be931813810
alepalacio <alejandropalacio24@gmail.com>
2023-09-24 17:53:04 -0300

    renamed fields for departments

--- custom_addons/custom_addons_hr_inta/models/hr_custom_department.py
+++ custom_addons/custom_addons_hr_inta/models/hr_custom_department.py
@@ -5,6 +5,8 @@ class HrCustomDepartment(models.Model):
     
     _inherit = 'hr.department'
 
+    name = fields.Char('Nombre Unidad', required=True)
+    parent_id = fields.Many2one(string="Unidad padre")
     tipo_unidad_id = fields.Many2one(
         'hr.department.tipo_unidad', 
         string='Tipo Unidad'
